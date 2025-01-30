11d5101c47d511ae2a191a566cfbc3c32a85d0dd
korver58 <ktokyoforest@yahoo.co.jp>
2025-01-30 23:25:42 +0900

    update

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 SCTIPR_DIR=$(cd $(dirname $0);pwd)
 cd ${SCTIPR_DIR}
 
-mkdir -p volumes/repository
+mkdir -p volumes/fossology
 mkdir -p volumes/postgresql/data
 
 sudo chown -R 1000:1000 ./volumes
 sudo chown -R 999:999 ./volumes/postgresql
-chmod -R 755 ./volumes
+sudo chmod -R 755 ./volumes
